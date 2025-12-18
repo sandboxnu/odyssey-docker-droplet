b47740a080e10bb9df6733166ec59f16e7e91d6a
mattrwang <wang.mat@northeastern.edu>
2025-12-18 15:07:53 -0500

    exercise setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const handleClick = () => {
+    fetch("http://localhost:5000/click")
+      .then((response) => response.json())
+      .then((data) => setText(data.message));
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +17,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={handleClick}>Click Me</button>
+        <p>{text}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/click", (req, res) => res.json({ message: "Hello from backend!" }));
+app.listen(5000, () => console.log("Server running on port 5000"));
